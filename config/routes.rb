f4710876e50e9239ef689fecbd92ea6a3578d727
Maxim Glukhov <maximiliano18213@gmail.com>
2021-08-31 00:05:18 +0500

    Add admin page with the list of users

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
   resources :questions do
     resources :answers, only: %i[create edit update destroy]
   end
+
+  namespace :admin do
+    resources :users, only: %i[index]
+  end
 end
